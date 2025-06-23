a1e9a21ea7fd06f139e1bd729fa7df45d0169ea0
alghozaliii <alghozali4331@student.uns.ac.id>
2025-06-23 20:20:11 +0700

    jali hc 9

--- .github/workflows/startup.sh
+++ .github/workflows/startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cd /home/site/wwwroot/public
-php -S 0.0.0.0:8080
+cd /home/site/wwwroot
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php -S 0.0.0.0:8080 index.php
