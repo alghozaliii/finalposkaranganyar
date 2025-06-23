6e0cf3105daae17273944eeef6f52bcc18fa41bf
alghozaliii <alghozali4331@student.uns.ac.id>
2025-06-23 20:58:08 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 cd /home/site/wwwroot
+
+# Bersihkan dan cache ulang konfigurasi Laravel
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php -S 0.0.0.0:8080 index.php
+
+# Jalankan Laravel dari folder public
+php -S 0.0.0.0:8080 -t public
